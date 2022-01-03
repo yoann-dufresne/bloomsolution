cccc043bb78895a1d6cfc8b4b106eeb371c13eec
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-01-03 23:59:45 +0100

    initial kmer manipulator

---
+++ kmer.cpp
@@ -0,0 +1,48 @@
+#include "kmer.hpp"
+
+
+KmerManipulator::KmerManipulator(uint64_t k) : k(k) {
+  // Create a mask of size (k-1) * 2 bits
+  // If k = 3 => mask = 001111
+  this->mask = (1 << (2 * (k-1))) - 1;
+
+  this->current_kmer = 0;
+  this->current_rev = 0;
+}
+
+
+void construct_next(char nucl) {
+  // --- Forward k-mer ---
+  // Mask the kmer to remove the first nucleotide
+  this->current_kmer &= this->mask;
+  // Shift the current kmer to the left to have 2 bits available on the right for the new nucleotide
+  this->current_kmer <<= 2;
+  // New nucleotide
+  uint64_t bin_nucl = 0;
+  switch(nucl) {
+    case 'A': bin_nucl=0; break;
+    case 'C': bin_nucl=1; break;
+    case 'G': bin_nucl=2; break;
+    case 'T': bin_nucl=3; break;
+  }
+  this->current_kmer |= bin_nucl;
+
+  // --- Reverse complement k-mer ---
+  // No need to mask the rev. The shift will destroy the 2 bits on the right
+  // Shift the current kmer to the right to have 2 bits available on the left for the new nucleotide
+  this->current_kmer >>= 2;
+  // New nucleotide
+  bin_nucl = 0;
+  switch(nucl) {
+    case 'A': bin_nucl=3; break;
+    case 'C': bin_nucl=2; break;
+    case 'G': bin_nucl=1; break;
+    case 'T': bin_nucl=0; break;
+  }
+  // Shift the nucleotide to be on the leftmost position
+  bin_nucl <<= 2 * (k - 1);
+  // Merge nucleotide
+  this->current_kmer |= bin_nucl;
+}
+
+#endif
---
+++ kmer.hpp
@@ -0,0 +1,28 @@
+
+#ifndef KMER_H
+#define KMER_H
+
+class KmerManipulator {
+private:
+	uint64_t k;
+	uint64_t mask;
+
+public:
+	// Where the current kmer is stored
+	uint64_t current_kmer;
+	// Where the current reverse complement kmer is stored
+	uint64_t current_rev;
+
+	/** Constructor for a kmer manipulator with the encoding A:0, C:1, G:2, T:3
+	 * Always keeps up to date the current kmer and its reverse complement
+	 */
+	KmerManipulator(uint64_t k);
+	/** Construct a kmer from the previous one + the last char.
+	 * 1 - Remove the useless nucleotide by masking + shifting
+	 * 2 - Add the new nucleotide
+	 * @param nucl The new nucleotide to add
+	 **/
+	void construct_next(char nucl);
+};
+
+#endif
