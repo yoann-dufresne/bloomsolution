7cdc5f5da5fbf3b300a4a47731e6ddc495213738
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-01-04 00:25:52 +0100

    kmer stream ok

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 
 #include "seq_reader.hpp"
+#include "kmer.hpp"
 
 using namespace std;
 
 
 int main(int argc, char const *argv[]) {
+	// Parameters
 	string filename = "data/test.fasta";
+	uint64_t k = 3;
 
-	SeqReader reader(filename);
+	// Init objects and useful variables
 	char c = 0;
+	SeqReader reader(filename);
+	KmerManipulator manip(k);
+
+	// Init the first (k-1)-mer
+	for (uint i=0 ; i<k-1 ; i++) {
+		c = reader.next();
+		cout << c << " ";
+		manip.construct_next(c);
+		cout << manip.current_kmer << " " << manip.current_rev << endl;
+	}
+	cout << endl;
+
+	// Process the data
 	while ((c = reader.next())) {
-		cout << c;
+		cout << c << " ";
+		manip.construct_next(c);
+		cout << manip.current_kmer << " " << manip.current_rev << endl;
 	}
 	cout << endl;
 
